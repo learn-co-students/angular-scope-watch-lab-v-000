310337f48990c37707be91fdb05baaf87b6dc168
onyoo <roberto9m9@gmail.com>
2016-03-25 12:10:08 +0100

    Done.

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -1,11 +1,38 @@
-function UserController() {
+function UserController($scope) {
+	var user = this;
+
 	this.username = '';
 
 	this.country = {
 		name: {
-
+			short: '',
+			full: '',
+			population: ''
 		}
 	};
+
+// watch username
+	$scope.$watch(function() {
+		// value returned to controller
+		return user.username;
+	}, function(newValue, oldValue) {
+		// executed on dirty
+		console.log(user.username);
+	});
+
+// deep watch
+	$scope.$watch(function() {
+		return user.country;
+	}, function(newVal,oldVal) {
+		console.log('Deep Watch Alert')
+	}, true)
+
+	$scope.$watchCollection(function() {
+		return [user.country.name.short, user.country.name.full];
+	}, function(newVal, oldVal) {
+		console.log('watchCollection '+ oldVal + ' -> ' + newVal)
+	}, true)
+
 }
 
 angular
