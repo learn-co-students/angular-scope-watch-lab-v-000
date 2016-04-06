242063ae46fc715d04dda2cc9256cbb1aa38bb8d
tuckerbohman5 <tucker.bohman@gmail.com>
2016-04-05 23:37:40 -0600

    Done.

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -1,11 +1,32 @@
 function UserController() {
-	this.username = '';
+	var ctrl = this;
+  ctrl.username = '';
 
-	this.country = {
-		name: {
+  $scope.$watch(function(){
+    return ctrl.username; 
+  }, function (newValue, oldValue){
+    console.log('value updated');
+  });
 
-		}
-	};
+	ctrl.country = {
+    name: {
+      short: '',
+      full: '',
+    },
+    population: ''
+  };
+
+  $scope.$watch(function(){
+    return ctrl.country;
+  }, function (newValue, oldValue){
+    console.log('value upadated');
+  }, true);
+
+  $scope.$watchCollection(function(){
+    return [ctrl.country.name.short, ctrl.country.name.full];
+  }, function (newValue, oldValue){
+    console.log('value updated');
+  }, true);
 }
 
 angular
