8b8191c5d78048e9fa1b561f15ad77e2afadc47c
John Anthony Rivera <askinganthony@gmail.com>
2016-04-06 13:50:24 -0700

    Done.

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -1,11 +1,33 @@
 function UserController() {
-	this.username = '';
+	var ctrl = this;
+	ctrl.username = '';
 
-	this.country = {
+	ctrl.country = {
 		name: {
-
+			fullName: 'United States',
+			shortName: 'US'
 		}
 	};
+
+
+	$scope.$watch(function () {
+      return ctrl.username;
+  }, function (newValue, oldValue) {
+      console.log('username updated from '+oldValue+' to '+newValue);
+  });
+
+	$scope.$watch(function () {
+      return ctrl.country;
+  }, function (newValue, oldValue) {
+      console.log('country updated!');
+  });
+
+	$scope.$watchCollection(function () {
+      return [ctrl.country.fullName, ctrl.country.shortName];
+  }, function (newValue, oldValue) {
+      console.log('country name updated!');
+  });
+
 }
 
 angular
