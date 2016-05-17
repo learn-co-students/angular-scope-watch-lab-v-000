1103087ce2b42c1ba93de8848f6f4be6e5d5d454
Robert <robmcarrier@gmail.com>
2016-05-16 21:29:08 -0700

    Done.

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -1,11 +1,32 @@
-function UserController() {
-	this.username = '';
 
-	this.country = {
-		name: {
+function UserController($scope) {
+var ctrl = this;
 
-		}
-	};
+ctrl.username = '';
+
+ctrl.country = {
+name: {
+
+}
+};
+
+$scope.$watch(function () {
+return ctrl.username;
+}, function (newValue, oldValue) {
+console.log('Username updated', newValue);
+});
+
+$scope.$watch(function () {
+return ctrl.country;
+}, function (newValue) {
+console.log('Country info updated');
+}, true);
+
+$scope.$watchCollection(function () {
+return ctrl.country.name;
+}, function (newValue) {
+console.log('Country name updated');
+});
 }
 
 angular
