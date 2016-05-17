116889f0a58cbb512c41f984d851485d3b090ab5
azheng249 <andy.zheng249@gmail.com>
2016-05-17 17:09:00 -0400

    Done.

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -1,11 +1,33 @@
-function UserController() {
-	this.username = '';
+function UserController($scope) {
+  var ctrl = this;
+
+	this.username = 'username';
 
 	this.country = {
 		name: {
-
-		}
+      shortName: 'USA',
+      fullName: 'United States of America'
+		},
+    population: 1
 	};
+
+  $scope.$watch(function () {
+    return ctrl.username;
+  }, function (newValue, oldValue) {
+    console.log("username updated");
+  });
+
+  $scope.$watch(function () {
+    return ctrl.country;
+  }, function (newValue, oldValue){
+    console.log("country updated");
+  }, true);
+
+  $scope.$watchCollection(function (){
+    return ctrl.country.name;
+  }, function (newValue, oldValue) {
+    console.log("country name updated");
+  });
 }
 
 angular
