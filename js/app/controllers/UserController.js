d641972779a008eb2e70748e443312b8f8257e3e
febbraiod <donedesign1@gmail.com>
2016-05-19 12:49:18 -0400

    Done.

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -6,8 +6,33 @@ function UserController() {
 
 		}
 	};
+
+  $scope.$watch(function(){
+    return this.username;
+  }, function (newValue, oldValue){
+    console.log('username updated');
+  }
+
+  );
+
+  $scope.$watch(function () {
+    return ctrl.country;
+  }, function (newValue, oldValue) {
+    console.log('value updated');
+  }, true);
+
+
+
+  $scope.$watchCollection(function () {
+      return this.country.name;
+  }, function (newValue, oldValue) {
+      console.log('value updated!');
+  });
+
+
 }
 
 angular
 	.module('app')
 	.controller('UserController', UserController);
+
