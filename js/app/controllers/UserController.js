3e5b0fe2bcc0127fbf0e0a55439e24144a8c959c
Brian <useyourbrian@gmail.com>
2016-05-18 20:17:17 -0400

    Done.

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -1,4 +1,24 @@
-function UserController() {
+ function UserController($scope) {
+	 var ctrl = this;
+
+	 $scope.$watch(function(){
+		 return ctrl.username;
+	 }, function(newValue, oldValue){
+		 console.log('Username updated!');
+	 });
+
+	 $scope.$watchCollection(function(){
+		 return ctrl.country;
+	 }, function(newValue, oldValue){
+		 console.log('Country updated!');
+	 }, true);
+
+	 $scope.$watchCollection(function(){
+		 return ctrl.country.name;
+	 }, function(newValue, oldValue){
+		 console.log('Country name updated!')
+	 });
+
 	this.username = '';
 
 	this.country = {
