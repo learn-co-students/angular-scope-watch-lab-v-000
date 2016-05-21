c2067b406c1a77ee4af01d73782c098ad5c22950
c1505 <coreym247@gmail.com>
2016-05-21 14:11:22 -0400

    Done.

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -1,11 +1,30 @@
-function UserController() {
-	this.username = '';
+function UserController($scope) {
+	var ctrl = this;
+  this.username = '';
 
-	this.country = {
+	ctrl.country = {
 		name: {
 
 		}
 	};
+
+  $scope.$watch(function () {
+    return ctrl.username;
+  }, function (newValue, oldValue) {
+    console.log('value updated');
+  });
+
+  $scope.$watch(function () {
+    return ctrl.country;
+  }, function (newValue, oldValue) {
+    console.log('value updated');
+  }, true);
+
+  $scope.$watchCollection(function () {
+    return ctrl.country.name;
+  }, function (newValue, oldValue) {
+    console.log('value updated!');
+  });
 }
 
 angular
