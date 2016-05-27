b5848a8f01438b385dccde201b92a08d15199507
Alvinluipo <lualvin1234@gmail.com>
2016-05-27 03:55:31 -0400

    Done.

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -1,13 +1,33 @@
 function UserController() {
-	this.username = '';
+  var username = this.username = '';
 
-	this.country = {
-		name: {
+  var country = this.country = {
+    name: {
 
-		}
-	};
+    }
+  };
+
+  $scope.$watch(function () {
+      return username;
+  }, function (newValue, oldValue) {
+      console.log('value updated!');
+  });
+
+
+  $scope.$watch(function () {
+    return country;
+  }, function (newValue) {
+    console.log('Country info updated');
+  }, true);
+
+  $scope.$watchCollection(function () {
+      return country;
+  }, function (newValue, oldValue) {
+      console.log('value updated!');
+  });
+
 }
 
 angular
-	.module('app')
-	.controller('UserController', UserController);
+  .module('app')
+  .controller('UserController', UserController);
