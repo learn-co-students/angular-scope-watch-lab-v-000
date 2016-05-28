0cc9bfa9b9e9fd54673f42e5cb5f21ee1b65113a
Enoch Griffith <enoch2k2@hotmail.com>
2016-05-28 15:42:08 -0500

    Done.

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -1,11 +1,32 @@
-function UserController() {
-	this.username = '';
+function UserController($scope) {
+	var user = this;
+	user.username = 'enoch2k2';
 
-	this.country = {
+	user.country = {
 		name: {
-
-		}
+			abbr: 'usa',
+			fullName: 'United States of America'
+		},
+		population: 'alot'
 	};
+
+	$scope.$watch(function(){
+		return user.username;
+	}, function(newValue, oldValue) {
+		console.log('value updated!');
+	});
+
+	$scope.$watch(function(){
+		return user.country;
+	}, function(newValue, oldValue){
+		console.log('country deep value updated!!')
+	}, true);
+
+	$scope.$watchCollection(function(){
+		return user.country;
+	}, function(newValue, oldValue){
+		console.log('collection updated!')
+	});
 }
 
 angular
