d431432a9b9a12b8e492bf37e3dd08ecfdb2c319
authorbeard <msnolan@gmail.com>
2016-06-03 11:41:07 -0400

    done

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -1,11 +1,32 @@
-function UserController() {
-	this.username = '';
+function UserController($scope) {
+	var user=this
+  user.username = '';
 
 	this.country = {
 		name: {
-
-		}
+        short: 'deez',
+        full: 'nuts',
+        },
+    population: 5000000
 	};
+
+  user.country=this.country
+  
+  $scope.$watch (function(){
+      return user.username
+  }, function(newValue, oldValue){
+      console.log("changed from " + oldValue + " to " + newValue)
+  })
+
+  $scope.$watch (function(){
+      return user.country
+  }, function(newValue, oldValue){
+    // debugger;
+      var nv=JSON.stringify(newValue) 
+      var ov=JSON.stringify(oldValue)
+      console.log("changed from " + ov + " to " + nv)
+  }, true)
+
 }
 
 angular
