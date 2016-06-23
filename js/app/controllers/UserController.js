3bd7e04225e948a9fdbff825fa8a7b6e32c3f133
mhnguyen289 <mhnguyen289@gmail.com>
2016-06-22 18:01:00 -0700

    Done.

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -1,4 +1,6 @@
-function UserController() {
+function UserController($scope) {
+	var user = this
+
 	this.username = '';
 
 	this.country = {
@@ -6,6 +8,25 @@ function UserController() {
 
 		}
 	};
+
+	$scope.$watch(function () {
+		return ctrl.username;
+	}, function (newValue, oldValue) {
+		console.log('value updated!');
+	});
+
+		$scope.$watch(function() {
+		return user.country;
+	}, function(newValue) {
+		console.log('Country has been updated');
+	}, true);
+
+	$scope.$watchCollection(function() {
+		return user.country.name;
+	}, function(newValue) {
+		console.log('Countries name is updated!');
+	});
+
 }
 
 angular
