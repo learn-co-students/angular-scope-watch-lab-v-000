8735420e09ca60fee0c479161d555d88bcf770c1
McN33ly <rmcneely23@gmail.com>
2016-08-02 17:37:04 -0400

    Done.

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -1,11 +1,28 @@
-function UserController() {
+function UserController($scope) {
+	var user = this
+
 	this.username = '';
 
 	this.country = {
 		name: {
-
+			short: '',
+			full: '',
+			population: ''
 		}
 	};
+
+	$scope.$watch(function (){
+		return user.user
+	}, function (oldValue, newValue){
+		console.log('Username Changed!')
+	})
+
+	$scope.$watch(function (){
+		return user.country
+	}, function(oldValue, newValue){
+		console.log('Country Changed!')
+	}, true)
+
 }
 
 angular
