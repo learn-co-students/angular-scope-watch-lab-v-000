e5aa4c1155fff9ad94d1252771288ea96ebbbc4a
Peter Jacobson <github email address>
2016-08-21 15:45:40 -0400

    Done.

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -6,6 +6,26 @@ function UserController() {
 
 		}
 	};
+
+    $scope.$watch(function () {
+    return user.username;
+  }, function (newValue. OldValue) {
+    console.log(‘value updated!’);
+  });
+
+       $scope.$watch(function () {
+    return user.country;
+  }, function (newValue. OldValue) {
+    console.log(‘value updated!’);
+  },true);
+
+        $scope.$watchCollection(function () {
+        return user.country.name;
+    }, function (newValue, oldValue) {
+        console.log('value updated!');
+    });
+
+
 }
 
 angular
