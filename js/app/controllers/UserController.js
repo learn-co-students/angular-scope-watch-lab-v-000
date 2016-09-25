f2db75ee2ee69ff446d152d8902364a080478ac7
James Novak <jamesnovak90@gmail.com>
2016-09-24 21:51:49 -0400

    Done.

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -1,11 +1,26 @@
-function UserController() {
+function UserController($scope) {
+  var ctrl = this
 	this.username = '';
+
+  $scope.$watch(function(){
+    return ctrl.username
+  }, function(newValue, oldValue){
+    console.log("value changed")
+  })
 
 	this.country = {
 		name: {
+      short: '',
+      full: ''
+		},
+    population: ''
+	}
 
-		}
-	};
+  $scope.$watch(function(){
+    return ctrl.country
+  }, function(newValue, oldValue) {
+    console.log('value changed')
+  }, true)
 }
 
 angular
