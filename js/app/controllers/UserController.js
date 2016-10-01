586f224b1ffba81308f8c129d956412fda140e06
aprietof <adrianprieto00@gmail.com>
2016-09-30 20:26:39 -0400

    Done.

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -1,44 +1,44 @@
 function UserController($scope) {
-	var user = this;
-	user.username = '';
+    var user = this;
+    user.username = '';
 
-	user.country = {
-		name: {
-			short: '',
-			full: ''
-		},
-		population: ''
-	};
+    user.country = {
+        name: {
+            short: '',
+            full: ''
+        },
+        population: ''
+    };
 
-	// watch username change
-	$scope.$watch(function () {
-      return user.username;
-  }, function (newValue, oldValue) {
-      console.log('username updated!');
-  });
+    // watch username change
+    $scope.$watch(function() {
+        return user.username;
+    }, function(newValue, oldValue) {
+        console.log('username updated!');
+    });
 
-	// Deep watch
-	$scope.$watch(function () {
-      return user.country;
-  }, function (newValue, oldValue) {
-      console.log('Country general info updated!');
-  }, true);
+    // Deep watch
+    $scope.$watch(function() {
+        return user.country;
+    }, function(newValue, oldValue) {
+        console.log('Country general info updated!');
+    }, true);
 
-	// Only watch change in country name
-	$scope.$watchCollection(function () {
-		return user.country.name;
-	}, function (newValue, oldValue) {
-		console.log('Country name Updated');
-	});
+    // Only watch change in country name
+    $scope.$watchCollection(function() {
+        return user.country.name;
+    }, function(newValue, oldValue) {
+        console.log('Country name Updated');
+    });
 
-	// Only watch change in country population
-	$scope.$watchCollection(function () {
-		return user.country.population;
-	}, function (newValue, oldValue) {
-		console.log('Country population Updated');
-	});
+    // Only watch change in country population
+    $scope.$watchCollection(function() {
+        return user.country.population;
+    }, function(newValue, oldValue) {
+        console.log('Country population Updated');
+    });
 }
 
 angular
-	.module('app')
-	.controller('UserController', UserController);
+    .module('app')
+    .controller('UserController', UserController);
