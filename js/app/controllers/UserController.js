cacbf2771ac1b05041b745391a00583c4b983923
aprietof <adrianprieto00@gmail.com>
2016-09-30 20:20:07 -0400

    Done.

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -1,11 +1,42 @@
-function UserController() {
-	this.username = '';
+function UserController($scope) {
+	var user = this;
+	user.username = '';
 
-	this.country = {
+	user.country = {
 		name: {
-
-		}
+			short: '',
+			full: ''
+		},
+		population: ''
 	};
+
+	// watch username change
+	$scope.$watch(function () {
+      return user.username;
+  }, function (newValue, oldValue) {
+      console.log('username updated!');
+  });
+
+	// Deep watch
+	$scope.$watch(function () {
+      return user.country;
+  }, function (newValue, oldValue) {
+      console.log('Country general info updated!');
+  }, true);
+
+	// Only watch change in country name
+	$scope.$watchCollection(function () {
+		return user.country.name;
+	}, function (newValue, oldValue) {
+		console.log('Country name Updated');
+	});
+
+	// Only watch change in country population
+	$scope.$watchCollection(function () {
+		return user.country.population;
+	}, function (newValue, oldValue) {
+		console.log('Country population Updated');
+	});
 }
 
 angular
