fe40a522d05fab6978f888bb45a340def17062b4
evanscloud <dijkstrascloud@gmail.com>
2016-10-11 13:33:14 -0400

    Add  to controller

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -1,11 +1,33 @@
-function UserController() {
-	this.username = '';
+function UserController($scope) {
+	var ctrl = this;
 
-	this.country = {
-		name: {
+	ctrl.ctrlname = '';
 
-		}
+	ctrl.country = {
+			name: {
+					short: 'US',
+					full: 'United States',
+					population: '318.9 million';
+			}
 	};
+
+	$scope.$watch(function () {
+			return ctrl.username;
+	}, function (newValue, oldValue) {
+			console.log("Username updated.", newValue);
+	});
+
+	$scope.$watch(function () {
+			return ctrl.country;
+	}, function (newValue) {
+			console.log("Country info updated.");
+	});
+
+	$scope.$watchCollection(function () {
+			return ctrl.country.full;
+	}, function (newValue) {
+			console.log("Country updated.");
+	});
 }
 
 angular
