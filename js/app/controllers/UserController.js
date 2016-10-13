1236cf3c1ca69771774f4fc4b90233bb6664decf
gabe <gj+code@mail.co.de>
2016-10-12 23:04:50 -0400

    Done.

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -1,4 +1,6 @@
-function UserController() {
+function UserController($scope) {
+  var ctrl = this;
+
 	this.username = '';
 
 	this.country = {
@@ -6,6 +8,24 @@ function UserController() {
 
 		}
 	};
+
+  $scope.$watch(function() {
+    return ctrl.username;
+  }, function(newVal, oldVal) {
+    console.log('Username was updated!', newVal, oldVal);
+  });
+
+  $scope.$watch(function() {
+    return ctrl.country;
+  }, function(newVal, oldVal) {
+    console.log('Some attribute of country was updated!', newVal, oldVal);
+  }, true);
+
+  $scope.$watchCollection(function() {
+    return ctrl.country.name;
+  }, function(newVal, oldVal) {
+    console.log('Country name was updated!', newVal, oldVal);
+  });
 }
 
 angular
