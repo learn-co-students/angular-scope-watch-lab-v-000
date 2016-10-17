86cf3adcb609559866b1504ca4521d3da131c0bc
Saturn226 <serenehopes@gmail.com>
2016-10-17 12:41:46 -0400

    Done.

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -1,11 +1,36 @@
 function UserController() {
-	this.username = '';
+  var ctrl = this;
+	ctrl.username = '';
 
-	this.country = {
+	ctrl.country = {
 		name: {
 
 		}
 	};
+
+
+
+  $scope.$watch(function () {
+    return ctrl.username;},
+    function (newValue, oldValue){
+      console.log('value updated!');
+  });
+
+  $scope.$watch(function () {
+    return ctrl.country;
+  }, function(newValue, oldValue) {
+    console.log('value updated!');
+  }, true);
+
+  $scope.$watchCollection(function () {
+    return ctrl.country; },
+    function (newValue, oldValue){
+      console.log('value updated!');
+    });
+
+}
+
+  
 }
 
 angular
