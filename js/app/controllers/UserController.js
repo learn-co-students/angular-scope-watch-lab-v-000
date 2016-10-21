c8dbf2807b48e275da4c7b56b5b0c94007e5d719
wedesignstudios <deelden@gmail.com>
2016-10-21 10:26:40 -0500

    Done

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -1,11 +1,38 @@
-function UserController() {
-	this.username = '';
+function UserController($scope) {
+	var ctrl = this;
+	ctrl.username = '';
 
-	this.country = {
+	ctrl.country = {
 		name: {
-
+			
 		}
 	};
+
+	ctrl.countryCollection = {
+		name: {
+			short: '',
+			full: ''
+		}
+	};
+
+	$scope.$watch(function() {
+			return ctrl.username;
+		}, function(newValue, oldValue) {
+			console.log(`New USERNAME value: ${newValue}`);
+		});
+
+	$scope.$watch(function() {
+			return ctrl.country;
+		}, function(newValue, oldValue) {
+			console.log(`New COUNTRY value: ${newValue}`);
+		}, true);
+
+	$scope.$watchCollection(function () {
+	        return ctrl.countryCollection;
+	    }, function (newValue, oldValue) {
+	        console.log(`New CountryCollection value: ${newValue}`);
+	    });
+
 }
 
 angular
