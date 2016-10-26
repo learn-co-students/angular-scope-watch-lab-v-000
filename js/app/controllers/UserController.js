7ab76a166526578ba866f4f47f7573545ec2649a
mcasciato <michael.casciato@icloud.com>
2016-10-26 11:51:14 -0400

    done

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -1,11 +1,31 @@
 function UserController() {
-	this.username = '';
+	var ctrl = this;
 
-	this.country = {
+	ctrl.username = '';
+
+	ctrl.country = {
 		name: {
 
 		}
 	};
+
+	$scope.$watch(function () {
+		return ctrl.username;
+	}, function (newValue, oldValue) {
+		console.log('value updated!');
+	});
+
+	$scope.$watch(function () {
+		return ctrl.country;
+	}, function(newValue, oldValue) {
+		console.log('value updated!');
+	}, true);
+
+	$scope.$watchCollection(function () {
+		return ctrl.country;
+	}, function(newValue, oldValue) {
+		console.log('value updated!');
+	});
 }
 
 angular
