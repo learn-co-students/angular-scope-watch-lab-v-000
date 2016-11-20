4a60915e17b644967ab1c40c6a1729d86df86782
auranbuckles <auran59@gmail.com>
2016-11-20 11:23:54 -0500

    Done.

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -1,13 +1,36 @@
 function UserController() {
-	this.username = '';
+	var ctrl = this;
+	ctrl.username = '';
 
-	this.country = {
+	ctrl.country = {
 		name: {
 
 		}
 	};
+
+	$scope.$watch(function () {
+		return ctrl.search;
+	}, function (newValue, oldValue) {
+		console.log('value updated!')
+	});
+
+	$scope.$watch(function () {
+		return ctrl.country;
+	}, function (newValue, oldValue) {
+		console.log('value updated!');
+	}, true);
+
+	$scope.$watchCollection(function () {
+		return ctrl.country;
+	}, function (newValue, oldValue) {
+		console.log('value updated!')
+	});
 }
 
 angular
 	.module('app')
 	.controller('UserController', UserController);
+
+// Using $scope.$watch, check whenever the username gets updated.
+// Deep watch the whole country object to see when any value in it changes (including the country's short and full names).
+// Use $watchCollection to see only when the country's full and short names are updated.
