7739993fbc7aba6dd5a710d5d7415f9b77b4a5fa
michaelsimon <michael.simon@gmail.com>
2016-11-20 13:01:44 -0500

    Done.

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -1,13 +1,33 @@
-function UserController() {
-	this.username = '';
+function UserController($scope) {
+	var ctrl  = this;
+	ctrl.username = '';
 
-	this.country = {
+	ctrl.country = {
 		name: {
-
-		}
+			short: '',
+			full: ''
+		},
+		population: ''
 	};
-}
 
+	$scope.$watch(function() {
+		return ctrl.username;
+	}, function(newValue, oldValue){
+		console.log('username value updated!');
+	});
+
+	$scope.$watchCollection(function() {
+		return ctrl.country.name;
+	}, function(newValue, oldValue) {
+		console.log('country short/full updated');
+	});
+
+	$scope.$watch(function(){
+		return ctrl.country;
+	}, function(newValue, oldValue) {
+		console.log('country deep watch update');
+	}, true);
+}
 angular
 	.module('app')
 	.controller('UserController', UserController);
