1ac9c01c1f668d9cce9edfc4da887a659810f72e
Joseph Lim <joseph.d.lim@gmail.com>
2016-11-27 20:01:50 -0500

    Done.

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -1,11 +1,33 @@
-function UserController() {
-	this.username = '';
+function UserController($scope) {
+	var user = this;
+	user.username = '';
 
-	this.country = {
+	user.country = {
 		name: {
-
-		}
+			short: "",
+			full: ""
+		},
+		population: ""
 	};
+
+	$scope.$watch(function (){
+		return user.username;
+	}, function () {
+		console.log('username');
+		console.log(user.username);
+	});
+
+	$scope.$watch(function (){
+		return user.country;
+	}, function () {
+		console.log('country');
+	}, true);
+
+$scope.$watchCollection(function (){
+	return user.country;
+}, function () {
+	console.log('country');
+});
 }
 
 angular
