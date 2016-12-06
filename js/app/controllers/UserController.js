713927b9bb9188286ba6b5d37c48cec2917b07da
Adrian Dave <adrian@paradoxlogic.co>
2016-12-05 21:50:39 -0500

    Done.

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -1,11 +1,32 @@
-function UserController() {
-	this.username = '';
+function UserController($scope) {
+	var ctrl = this;
 
-	this.country = {
+	ctrl.username = '';
+
+	ctrl.country = {
 		name: {
 
 		}
 	};
+
+	$scope.$watch(function () {
+			 return ctrl.username;
+	 }, function (newValue, oldValue) {
+			 console.log('username value updated!');
+	 });
+
+	 $scope.$watch(function () {
+ 			 return ctrl.country;
+ 	 }, function (newValue, oldValue) {
+ 			 console.log('country info updated!');
+ 	 }, true);
+
+	 $scope.$watchCollection(function () {
+			 return ctrl.country.name;
+	 }, function (newValue, oldValue) {
+			 console.log('country name updated!');
+	 });
+
 }
 
 angular
