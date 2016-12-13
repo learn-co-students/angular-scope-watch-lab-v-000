5fdc09308e64716ba5d6ecfbee0f865d503a8546
Samuel Lair <lair001@gmail.com>
2016-12-13 03:40:48 -0500

    Done.

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -1,11 +1,32 @@
-function UserController() {
-	this.username = '';
+function UserController($scope) {
 
-	this.country = {
+	var vm = this;
+
+	vm.username = '';
+
+	vm.country = {
 		name: {
 
 		}
 	};
+
+	$scope.$watch(function() {
+		return vm.username;
+	}, function(oldValue, newValue) {
+		console.log('Username changed from %s to %s.', newValue, oldValue);
+	});
+
+	$scope.$watch(function() {
+		return vm.country;
+	}, function(oldValue, newValue) {
+		console.log('Country info changed: %o to %o.', newValue, oldValue);
+	}, true);
+
+	$scope.$watchCollection(function() {
+		return vm.country.name
+	}, function(oldValue, newValue) {
+		console.log('Country name changed: %o to %o', newValue, oldValue);
+	});
 }
 
 angular
