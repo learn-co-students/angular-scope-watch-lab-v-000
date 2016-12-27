ef1d51006549120b2eed47157bb5812533692443
jbok4 <jbok4@aol.com>
2016-12-27 12:04:32 -0500

    complete

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -1,11 +1,33 @@
 function UserController() {
-	this.username = '';
+	var user = this;
+  user.username = 'Jaclyn Ciringione';
 
-	this.country = {
+	user.country = {
 		name: {
+      abbr: 'America',
+      fullName: 'The United States of America'
+    },
+    population: '330 Million'
+  };
 
-		}
-	};
+    $scope.$watch(function () {
+        return user.username;
+    }, function (newValue, oldValue) {
+        console.log('username updated!');
+    });
+
+    $scope.$watch(function () {
+        return user.country;
+    }, function (newValue, oldValue) {
+        console.log('deep watch value updated!');
+    }, true);
+
+    $scope.$watchCollection(function () {
+        return user.country.name;
+    }, function (newValue, oldValue) {
+        console.log('country updated!');
+
+  });
 }
 
 angular
