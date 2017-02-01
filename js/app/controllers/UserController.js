9b097d5ec068bd15807c5271c5134110c833d7cc
Christian Whitesides <whitesidescr@gmail.com>
2017-02-01 11:27:38 -0700

    Done.

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -1,11 +1,31 @@
-function UserController() {
+function UserController($scope) {
 	this.username = '';
 
 	this.country = {
 		name: {
-
-		}
+			short: '',
+			full: ''
+		},
+		population: ''
 	};
+
+	$scope.$watch(function (){
+		return this.username;
+	}, function(newValue, oldValue) {
+		console.log('username updated!');
+	});
+
+	$scope.$watch(function () {
+		return this.country;
+	}, function(newValue, oldValue) {
+		console.log('country updated!');
+	}, true);
+
+	$scope.$watchCollection(function () {
+    return this.collection;
+  }, function (newValue, oldValue) {
+    console.log('value updated!');
+  });
 }
 
 angular
