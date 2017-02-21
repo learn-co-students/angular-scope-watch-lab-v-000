9d366a993988a0b33cc2f6099e3c2eaac3278790
Laurent Milne <lmilne360@gmail.com>
2017-02-21 02:33:57 -0500

    Done.

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -1,12 +1,34 @@
-function UserController() {
+function UserController($scope) {
+	var ctrl = this;
 	this.username = '';
 
 	this.country = {
 		name: {
-
-		}
+			short: '',
+			full: ''
+		},
+		population: ''
 	};
+
+	$scope.$watch(function(){
+		return ctrl.username;
+	}, function(newValue, oldValue){
+		console.log('User updated!');
+	});
+
+	$scope.$watchCollection(function () {
+			 return ctrl.country.name;
+	 }, function (newValue, oldValue) {
+			 console.log('Name updated!');
+	 });
+
+	 $scope.$watch(function () {
+			 return ctrl.country;
+	 }, function (newValue, oldValue) {
+			 console.log('Object updated!');
+	 }, true);
 }
+
 
 angular
 	.module('app')
