7966c461e94a6f72ac66f468066cbeb7ba34531f
hilaryml <hilyttle@gmail.com>
2017-02-24 15:47:48 -0500

    Done.

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -6,6 +6,26 @@ function UserController() {
 
 		}
 	};
+
+	var ctrl = this;
+
+	$scope.$watch(function () {
+    return ctrl.username;
+  }, function (newValue, oldValue) {
+    console.log('username updated!');
+  });
+
+	$scope.$watch(function () {
+    return ctrl.country;
+  }, function (newValue, oldValue) {
+    console.log('country updated!');
+  }, true);
+
+	$scope.$watchCollection(function () {
+    return ctrl.country.name;
+  }, function (newValue, oldValue) {
+    console.log('country name updated!');
+  });
 }
 
 angular
