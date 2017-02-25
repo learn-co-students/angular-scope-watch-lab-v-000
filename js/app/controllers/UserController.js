262a7e1612aa41f72078b101708f1fb72aa866c8
Clare <clare.e.horton@gmail.com>
2017-02-25 00:18:41 -0600

    Done.

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -1,11 +1,32 @@
-function UserController() {
-	this.username = '';
+function UserController($scope) {
+	var user = this;
+	user.username = '';
 
-	this.country = {
+	user.country = {
 		name: {
-
-		}
+			short: "",
+			full: ""
+		},
+		population: ""
 	};
+
+	$scope.$watch(function () {
+		return user.username;
+	}, function (newValue, oldValue) {
+		console.log("Username was " + oldValue + ", now it's " + newValue);
+	});
+
+	$scope.$watch(function () {
+		return user.country;
+	}, function (newValue, oldValue) {
+		console.log(user.country);
+	}, true);
+
+	$scope.$watchCollection(function () {
+		return user.country.name;
+	}, function (newValue, oldValue) {
+		console.log(user.country.name);
+	});
 }
 
 angular
